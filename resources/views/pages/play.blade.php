9654764887f1822b08fe5eb66911a16b1706c7b7
Lukas St <lukasstauskas@yahoo.com>
2024-11-02 20:13:40 +0200

    Displaying actual pieces (not numbers) on chessboard

--- resources/views/pages/play.blade.php
+++ resources/views/pages/play.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.logged-user')
 
+@php
+    use App\Models\Chessboard;
+@endphp
+
 @section('content')
     <div class="flex justify-center">
         <div class="border rounded">
@@ -9,8 +13,8 @@
                         @php
                             $square_color = $file['square_color'] == 'lightCol' ? 'bg-amber-700' : 'bg-slate-100';   
                         @endphp
-                        <div class="{{ $square_color }} w-12 h-12">
-                            
+                        <div class="{{ $square_color }} w-12 h-12 flex justify-center items-center">
+                            <img src="{{ Chessboard::get_piece_icon($file['piece']) }}">
                         </div>
                     @endforeach
                 </div>
